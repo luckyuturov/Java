53a8ae772354e324aa965264ec30f69328123838
Taalaibek <lucky.uturov@gmail.com>
2023-07-03 21:39:47 +0600

    Phrase generator end of page 46

---
+++ src/main/java/chapter1/page46/PhraseOMatic.java
@@ -0,0 +1,23 @@
+package chapter1.page46;
+
+public class PhraseOMatic {
+    public static void main(String[] args) {
+        String[] wordListOne = {"круглосуточный", "трех-звенный", "30000-футовый", "взаимный", "обоюдный выигрыш", "фронтэнд", "на основе веб-технологий", "проникающий", "умный", "шесть сигм", "метод критического пути", "динамический"};
+        String[] wordListTwo = {"уполномоченный", "трудный", "чистый продукт", "ориентированный", "центральный", "распределенный", "кластеризованный", "фирменный", "нестандартный ум", "позиционированный", "сетевой", "сфокусированный", "использованный с выгодой", "выровненный", "нацеленный на", "общий", "совместный", "ускоренный"};
+        String[] wordListThree = {"процесс", "пункт разгрузки", "выход из положения", "тип структуры", "талант", "подход", "уровень завоеванного внимания", "портал", "период времени", "обзор", "образец", "пункт следования"};
+
+        //count word in each list
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        //generate three random number
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+
+        //generate phrase
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+        System.out.println("Все что нам нужно, - это " + phrase);
+    }
+}
